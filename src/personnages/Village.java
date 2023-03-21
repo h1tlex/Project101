760bd0626458569aaf47a63446eaf2ff37b55f65
h1tlex <89601876+h1tlex@users.noreply.github.com>
2023-03-21 22:29:54 +0100

    Fixed ajouterHabitant ; trouverHabitant

--- src/personnages/Village.java
+++ src/personnages/Village.java
@@ -1,18 +1,20 @@
 package personnages;
 
+import java.util.Iterator;
+
 public class Village {
 
 	private String nom;
 	private Chef chef;
 	private int nbVillageois = 0;
-	private int nbVillageoisMaximum;
-	private	Gaulois villageois[]= new Gaulois [nbVillageoisMaximum];
+	private int nbVillageoisMaximum=10;
+	private	Gaulois villageois[] = new Gaulois[nbVillageoisMaximum];
 	
 	
 	public Village(String nom, int nbVillageoisMaximum) {
 		this.nom = nom;
 		this.nbVillageoisMaximum= nbVillageoisMaximum;
-	}
+		}
 	
 	public Village(){}
 
@@ -25,13 +27,28 @@ public class Village {
 		return nom;
 	}
 	
+	public Gaulois getVillageois(int i) {
+		return villageois[i];
+	}
+	
 	public Gaulois[] ajouterHabitant(Gaulois gaulois) {
 		
-		for(int i=0;i<nbVillageois;i++) {
-			if(villageois[i] == null) {
-				villageois[i] = new Gaulois(gaulois.getNom(),gaulois.getForce(),gaulois.getEffetPotion());
+		
+		if (nbVillageois<nbVillageoisMaximum) {
+	
+			for(int i=0;i<nbVillageoisMaximum;i++) {
+				if(villageois[i] == null) {
+					villageois[i] = new Gaulois(gaulois.getNom(),gaulois.getForce(),gaulois.getEffetPotion());
+					nbVillageois++;
+					System.out.println("Le villageois " + gaulois.getNom() + " à était ajouté à la liste du village!");
+	                break; // exit the loop once the new villager has been added
+
+				}
 			}
+		} else {
+			System.out.println("Le villageois " + gaulois.getNom() + " n'a pas était ajouté à la liste du village!");
 		}
+	
 		return villageois;
 	}
 	
@@ -46,14 +63,38 @@ public class Village {
 		return null;
 	}
 	
+	public void printVillageois() {
+	    for (int i = 0; i < nbVillageois; i++) {
+	        System.out.println(villageois[i]);
+	    }
+	}
+	
+	@Override
+	public String toString() {
+		
+		return "Villageois [nom=" + nom + ", nbVillageois=" + nbVillageois + ", nbVillageoisMaximum=" + nbVillageoisMaximum + "]";
+	}
+	
+	
 	public static void main(String[] args) {
 		
 		int nbVillageoisMaximum=10;
 		Gaulois villageois[]=new Gaulois [nbVillageoisMaximum];
-	
+		int nbVillageois=0;
 	    Village village = new Village();
-	    Gaulois habitant = village.trouverHabitant(0);
-	    System.out.println(habitant);
+	    
+	    Gaulois habitant1 = new Gaulois("Asterix",10,2);
+	    village.ajouterHabitant(habitant1);
+	    
+	    Gaulois habitant2 = new Gaulois("Obelix",12,3);
+	    village.ajouterHabitant(habitant2);
+	    
+	    village.printVillageois();
+	    
+	    System.out.println("\n test trouver habitant :");
+	    for(int i=0; i<nbVillageoisMaximum;i++) {
+	    	System.out.println(village.trouverHabitant(i));
+		}
 		
 	}
 	
